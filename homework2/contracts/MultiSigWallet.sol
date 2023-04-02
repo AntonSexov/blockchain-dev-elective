db255d5da8b85e195d34496688a5919f68a92c81
AntonSexov <antonvedernikov2003@gmail.com>
2023-04-02 17:55:44 +0300

    contract ready

--- homework2/contracts/MultiSigWallet.sol
+++ homework2/contracts/MultiSigWallet.sol
@@ -1,2 +1,13 @@
 // SPDX-License-Identifier: WTFPL
 pragma solidity ^0.8.9;
+
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
+import "@openzeppelin/contracts/utils/math/SafeMath.sol";
+
+contract MultiSigWallet{
+    using SafeERC20 for IERC20;
+    using SafeMath for uint256;
+
+    
+}
